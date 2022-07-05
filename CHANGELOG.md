c14c5aeefda36cfe9390ab6d7063a73bb67143a0
Ivan Novikov <novikov.ivan.work@gmail.com>
2022-07-05 11:30:06 +0300

    updated deps, removed internal modifiers

--- CHANGELOG.md
+++
@@ -1,6 +0,0 @@
-Kotlin test changelog
-===========================================
-
-0.1.0
-------
-- Initial Release
--- src/main/kotlin/com/turbomates/testsupport/response/Kotlinx.kt
+++ src/main/kotlin/com/turbomates/testsupport/response/Kotlinx.kt
@@ -10,19 +10,19 @@ import kotlinx.serialization.json.JsonArray
 import kotlinx.serialization.json.JsonElement
 import kotlinx.serialization.serializer
 
-internal fun JsonArray.arrayContains(value: JsonElement) {
+fun JsonArray.arrayContains(value: JsonElement) {
     this shouldContain value
 }
 
-internal fun JsonArray.hasCount(count: Int) {
+fun JsonArray.hasCount(count: Int) {
     count() shouldBe count
 }
 
-internal suspend inline fun <reified T : JsonElement> HttpResponse.toJsonElement(): T {
+suspend inline fun <reified T : JsonElement> HttpResponse.toJsonElement(): T {
     return Json.decodeFromString(bodyAsText())
 }
 
 @Suppress("unchecked_cast")
-internal suspend inline fun <reified T> HttpResponse.mapTo(): T {
+suspend inline fun <reified T> HttpResponse.mapTo(): T {
     return Json.decodeFromString(serializer(), bodyAsText())
 }
